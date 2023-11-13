195270828714d2886c715a102a63c1d5541d97c7
Tiffany Perez <pereztb02@gmail.com>
2023-11-13 16:36:50 -0500

    finished readMe

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Logo.Now
+## ![Static Badge](https://img.shields.io/badge/License-MIT-yellow)
+
+## Description
+
+A command line app that generates simple logos in .svg file format.
+
+
+
+## Installation
+
+Requires Inquirer and a browser.
+
+## Usage
+
+
+
+## Citations
+Link to SVG tutorial: https://www.w3schools.com/graphics/svg_circle.asp
+
+
+## Questions?
+<p>If you have questions or suggestions please contact me via email at </p>
+
+<pereztb03@gmail.com>
+
+<p>Check out my GitHub! </p>
+
+<>
+
+## Licenses
+
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
--- index.js
+++ index.js
@@ -1,38 +1,72 @@
-const inquirer = require('inquirer');
-const jest = require('jest');
+const inquirer = require("inquirer");
+const { Triangle } = require("./lib/shapes");
+const { Square } = require("./lib/shapes");
+const { Circle } = require("./lib/shapes");
+
+const fs = require("fs");
+// const { error } = require("console");
+// const { ifError } = require("assert");
 
 inquirer
-.prompt([ //Array of questions for logo params
-{
-type: 'list',
-name: 'shapes',
-message: 'What shape would you like?',
-choices: ['circle', 'square', 'triangle']
-},
-{
-    type: 'input',
-    name: 'colors',
-    message: 'What color would you like?'
-},
-{
-    type: 'confirm',
-    name: 'text_confirm',
-    message: 'Would you like text on your logo?',
-    
-},
-{
-    type: 'input',
-    name: 'text',
-    message: 'Input logo text',
-    //only ask this question if the text_confirm was answered 'yes'.
-    when: (answers) => answers.text_confirm === true,
-    },    
-])
-.then((answers) => {
+  .prompt([
+    //Array of questions for logo params
+    {
+      type: "list",
+      name: "shapes",
+      message: "What shape would you like?",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      type: "input",
+      name: "colors",
+      message: "What color would you like?",
+    },
+    {
+      type: "confirm",
+      name: "text_confirm",
+      message: "Would you like text on your logo?",
+    },
+    {
+      type: "input",
+      name: "text_color",
+      message: "What color would you like your text to be?",
+      //only ask this question if the text_confirm was answered 'yes'.
+      when: (answers) => answers.text_confirm === true,
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "Input logo text",
+      //only ask this question if the text_confirm was answered 'yes'.
+      when: (answers) => answers.text_confirm === true,
+    },
+  ])
+  .then((answers) => {
+    let svg;
+    console.log(answers);
+    if (answers.shapes === "triangle") {
+      svg = new Triangle();
+    } else if (answers.shapes === "square") {
+      svg = new Square()
+    } else if (answers.shapes === "circle") {
+      svg = new Circle()
+    }
+
     if (answers.text_confirm) {
-        //if text_confirm is 'yes', and text is input, then message is sent
-        console.log("logo text recieved!");
-    } else if (answers.text) {
-            console.log("user does not want text");
+      //if text_confirm is 'yes', and text is input, then message is sent
+      console.log("logo text recieved!");
+      svg.setText(answers.text)
+    } else {
+      console.log("user does not want text");
+      svg.setText("")
+    }
+    svg.setColors(answers.colors);
+    svg.setText_color(answers.text_color);
+    fs.writeFile("./examples/logo.svg", svg.render(), (error) => {
+        if (error) {
+        console.log("no can do", error)
         }
-});
+        console.log("Generated logo.svg!")
+    })
+  });
+
